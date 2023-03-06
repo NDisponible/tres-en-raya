30cdc7ddc0db173765139f61305020b23fb62992
NDisponible <cursowebweb22@gmail.com>
2023-03-06 20:54:16 +0100

    subiendo proyecto

---
+++ src/components/ScoreBoard/ScoreBoard.js
@@ -0,0 +1,8 @@
+import './ScoreBoard.css';
+const ScoreBoard = ({scoreX, scoreO}) => (
+    <div className='score-board'>
+        <div>{scoreX}</div>
+        <div>{scoreO}</div>
+    </div>
+)
+export default ScoreBoard;
